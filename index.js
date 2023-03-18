be772958e0c59e36d520bd197c304777c103aafe
Sujitha1149 <sujithamannempalli779@gmail.com>
2023-03-18 05:21:23 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="head">Super Over League</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="pic"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="pic"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
